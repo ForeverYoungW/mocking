d3c1821a23a4943623d5219823bf2ba770fdea67
ForeverYoungW <463671217@qq.com>
2015-03-31 19:01:00 +0800

    Revert "css"
    This reverts commit bda1ace0f812e23f3804d4b8ce438ff0a8980188.

--- css练习/JavaScript.js
+++
@@ -1,5 +0,0 @@
-﻿$(document).ready(function () {
-    var a = $(".CenterPosition");
-    a.css("margin-top", "-" + a.height() / 2 + "px");
-    a.css("margin-left", "-", +a.width() / 2 + "px");
-});
